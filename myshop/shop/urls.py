addeaef8a4c20955ca323d541d78a9024b15b18b
Marina <marinchanka@yandex.ru>
2024-04-14 23:42:15 +0300

    py files, cart html

--- myshop/shop/urls.py
+++ myshop/shop/urls.py
@@ -2,7 +2,8 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(r'^$', views.product_list, name='product_list'),
+    url(r'^$', views.main, name='main'),
+    url(r'^catalogue$', views.product_list, name='product_list'),
     url(r'^(?P<category_slug>[-\w]+)/$',
         views.product_list,
         name='product_list_by_category'),
