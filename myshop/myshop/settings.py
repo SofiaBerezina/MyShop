f77e23185c5c0a5a42dfe6e72d5acd98a09d58f7
SofiaBerezina <sofiaber114@gmail.com>
2024-05-26 20:38:42 +0300

    Добавление товаров

--- myshop/myshop/settings.py
+++ myshop/myshop/settings.py
@@ -110,4 +110,6 @@ DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
 STATIC_URL = '/static/'
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
-STATIC_ROOT = os.path.join(BASE_DIR, "static/")
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "static"),
+]
